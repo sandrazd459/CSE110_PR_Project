1dc70a5a1937780562558c878f6d0800b4d98517
DarthWanda <julianlin@Julians-MacBook-Pro.local>
2016-11-29 19:31:31 -0800

    scenario test.

--- app/src/androidTest/java/org/example/blog/offer_new_ride_Test.java
+++ app/src/androidTest/java/org/example/blog/offer_new_ride_Test.java
@@ -1,6 +1,5 @@
 package org.example.blog;
 
-import java.lang.*;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -13,6 +12,7 @@ import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 import org.hamcrest.core.IsInstanceOf;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -35,40 +35,54 @@ import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class sell_post_activity_Test {
+public class offer_new_ride_Test {
 
     @Rule
     public ActivityTestRule<Welcome> mActivityTestRule = new ActivityTestRule<>(Welcome.class);
 
-    @Test
-    public void selltest() {
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.welcome_login), withText("Log in"), isDisplayed()));
-        appCompatButton.perform(click());
-        //Because the firebase will remember your session, this test may fail if the user is currently log in
-        //So run Logout_Test or disable the following code to solve the problem
-        //The following code is to loggLin
-        //#==================================================================#
-        ViewInteraction appCompatEditText = onView(
-                withId(R.id.email));
-        appCompatEditText.perform(scrollTo(), replaceText("shl455@ucsd.edu"), closeSoftKeyboard());
+//    @ClassRule
+//    public static DisableAnimationsRule disableAnimationsRule = new DisableAnimationsRule();
 
-        ViewInteraction appCompatEditText2 = onView(
-                withId(R.id.password));
-        appCompatEditText2.perform(scrollTo(), replaceText("c20070124"), closeSoftKeyboard());
+    public void login(){
+        try{
+            ViewInteraction appCompatButton = onView(
+                    allOf(withId(R.id.welcome_login), withText("Log in"), isDisplayed()));
+            appCompatButton.perform(click());
 
-        ViewInteraction appCompatButton2 = onView(
-                allOf(withId(R.id.sign_button), withText("Login")));
-        appCompatButton2.perform(scrollTo(), click());
+            ViewInteraction appCompatEditText = onView(
+                    withId(R.id.email));
+            appCompatEditText.perform(scrollTo(), replaceText("shl455@ucsd.edu"), closeSoftKeyboard());
 
+            ViewInteraction appCompatEditText2 = onView(
+                    withId(R.id.password));
+            appCompatEditText2.perform(scrollTo(), replaceText("c20070124"), closeSoftKeyboard());
+
+            ViewInteraction appCompatButton2 = onView(
+                    allOf(withId(R.id.sign_button), withText("Login")));
+            appCompatButton2.perform(scrollTo(), click());
+        }
+        catch (Exception e){}
+        //after log in button is pressed, it takes some times to load the main_navigation_Test navigation page
+        //and be ready to display contents
+        //So the thread need to sleep for a short time
         try {
-            // thread to sleep for 3000 milliseconds
-            Thread.sleep(3000);
+            // thread to sleep for 5000 milliseconds
+            Thread.sleep(5000);
         } catch (Exception e) {
             System.out.println(e);
         }
-        //#==================================================================#
-
+    }
+    @Test
+    public void offer_new_ride_test() {
+//        try {
+//            // thread to sleep for 5000 milliseconds
+//            Thread.sleep(5000);
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
+        //given the user is logged in and on the main_navigation_Test navigation page
+        login();
+        //when the user press offer New Ride button
         ViewInteraction appCompatButton3 = onView(
                 allOf(withId(R.id.sellingList), withText("Offer New Ride"),
                         withParent(allOf(withId(R.id.activity_main_navigation),
@@ -76,6 +90,7 @@ public class sell_post_activity_Test {
                         isDisplayed()));
         appCompatButton3.perform(click());
 
+        //then the offer ride page will be present and the user will be able to offer new ride
         ViewInteraction editText = onView(
                 allOf(withId(R.id.startText),
                         childAtPosition(
@@ -117,6 +132,23 @@ public class sell_post_activity_Test {
                         isDisplayed()));
         button2.check(matches(isDisplayed()));
 
+        logout();
+    }
+
+    public void logout(){
+        try {
+            ViewInteraction linearLayout = onView(
+                    allOf(withId(R.id.account),
+                            withParent(allOf(withId(R.id.bb_bottom_bar_item_container),
+                                    withParent(withId(R.id.bb_bottom_bar_outer_container)))),
+                            isDisplayed()));
+            linearLayout.perform(click());
+
+            ViewInteraction appCompatButton3 = onView(
+                    allOf(withId(R.id.logout_button), withText("Log Out"), isDisplayed()));
+            appCompatButton3.perform(click());
+        }
+        catch (Exception e){}
     }
 
     private static Matcher<View> childAtPosition(
--- app/src/androidTest/java/org/example/blog/view_request_list_Test.java
+++ app/src/androidTest/java/org/example/blog/view_request_list_Test.java
@@ -6,8 +6,13 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
-import org.hamcrest.core.IsInstanceOf;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,17 +29,16 @@ import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class Logout_Test {
+public class view_request_list_Test {
 
     @Rule
     public ActivityTestRule<Welcome> mActivityTestRule = new ActivityTestRule<>(Welcome.class);
 
-    //given the user is loged in and on main navigation page
+//        @ClassRule
+//    public static DisableAnimationsRule disableAnimationsRule = new DisableAnimationsRule();
+
     public void login(){
         try{
             ViewInteraction appCompatButton = onView(
@@ -52,7 +56,6 @@ public class Logout_Test {
             ViewInteraction appCompatButton2 = onView(
                     allOf(withId(R.id.sign_button), withText("Login")));
             appCompatButton2.perform(scrollTo(), click());
-
         }
         catch (Exception e){}
         //after log in button is pressed, it takes some times to load the main_navigation_Test navigation page
@@ -67,34 +70,65 @@ public class Logout_Test {
     }
 
     @Test
-    public void logout() {
+    public void view_request_list_Test() {
 //        try {
 //            // thread to sleep for 5000 milliseconds
 //            Thread.sleep(5000);
 //        } catch (Exception e) {
 //            System.out.println(e);
 //        }
-        //Given that the "shl455@ucsd.edu" account exists and is logged in
+        //given the user is logged in and on the main navigation page
         login();
 
-        //When the user goes to the account page
+        //when he/she click the list button on the bottombar
         ViewInteraction linearLayout = onView(
-                allOf(withId(R.id.account),
+                allOf(withId(R.id.lists),
                         withParent(allOf(withId(R.id.bb_bottom_bar_item_container),
                                 withParent(withId(R.id.bb_bottom_bar_outer_container)))),
                         isDisplayed()));
         linearLayout.perform(click());
 
-        //and hit log out
-        ViewInteraction appCompatButton3 = onView(
-                allOf(withId(R.id.logout_button), withText("Log Out"), isDisplayed()));
-        appCompatButton3.perform(click());
+        //and click request List
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.title), withText("Request List"), isDisplayed()));
+        appCompatTextView.perform(click());
 
-        //Then the welcome page is presented and shows the user to be logged out.
-        ViewInteraction button = onView(
-                allOf(withId(R.id.welcome_login),
+        //then the user will be able to see the request list
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.recyclerList),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.activity_setttings_),
+                                        0),
+                                1),
                         isDisplayed()));
-        button.check(matches(isDisplayed()));
+        recyclerView.check(matches(isDisplayed()));
+
+        ViewInteraction linearLayout2 = onView(
+                allOf(withId(R.id.home),
+                        withParent(allOf(withId(R.id.bb_bottom_bar_item_container),
+                                withParent(withId(R.id.bb_bottom_bar_outer_container)))),
+                        isDisplayed()));
+        linearLayout2.perform(click());
+
     }
 
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
 }
